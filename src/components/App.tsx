b118c66382a5216815f99b4a51769e4360eaca42
MyckollWinchester <myckoll.winchester@usm.cl>
2024-10-28 17:08:01 -0300

    base for migration ᓚᘏᗢ

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import hmhLogo from '/hmh-icon.svg'
-import '../stylesheets/App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/home_page'
+import NavBar from './nav_bar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="#">
-          <img src={hmhLogo} className="logo" alt="HMH logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>HelpMyHome + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the React logo to learn more
-      </p>
+      <BrowserRouter>
+        <div className='app'>
+          <NavBar />
+          <div className='layout__page'>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              {/* <Route path='/lightbulb' element={<LightbulbPage />} /> */}
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
